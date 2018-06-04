normalization fixed

bin/load_data.py
@@ -10,18 +10,22 @@ import os
 
 class load(object):
 
-    def __init__(self, fn, dirimg):
+    def __init__(self, fn, dirimg, dirjac):
         self.fn = fn
         self.dirimg = dirimg
+        self.dirjac = dirjac
         self.y_test0_id = []
         self.y_test0 =[]
         self.y_train0_id =[]
         self.y_train0 =[]
         self.train_data_matrix =[]
         self.test_data_matrix =[]
+        self.jac_train_data_matrix = []
+        self.jac_test_data_matrix =[]
 
         self.read_csv()
         self.load_img()
+        self.load_jacdet()
 
     def read_csv(self):
         ### Partitioning demographic data : train vs test
@@ -76,3 +80,33 @@ class load(object):
                     continue
         self.train_data_matrix = train_data_matrix
         self.test_data_matrix = test_data_matrix
+
+    def load_jacdet(self):
+        # print('hi')
+        for root, dirs, files in os.walk(self.dirjac):
+            num_train = self.y_train0_id.shape[0]  ###
+            num_test = self.y_test0_id.shape[0]  ###
+            jac_train_data_matrix = np.ones((num_train, 128, 64, 64))
+            jac_test_data_matrix = np.ones((num_test, 128, 64, 64))
+
+            train_i = 0
+            test_i = 0
+            for file in files:
+                #       subjID.append(file.split('.')[0])
+                #       path = os.path.join(root,dirs)
+                filename = self.dirimg + file
+
+                if np.isin(file[0:9], self.y_train0_id):  ###
+                    with open(filename, 'rb') as f:
+                        jac_train_data_matrix[train_i, :, :, :] = pickle.load(f)
+                        train_i += 1
+                elif np.isin(file[0:9], self.y_test0_id):
+                    with open(filename, 'rb') as f:
+                        jac_test_data_matrix[test_i, :, :, :] = pickle.load(f)
+                        test_i += 1
+                else:
+                    continue
+        self.jac_train_data_matrix = jac_train_data_matrix
+        self.jac_test_data_matrix = jac_test_data_matrix
+
+

bin/network_model.py
@@ -9,13 +9,14 @@ from keras.models import Model # basic class for specifying and training a neura
 from keras.layers import Input, Convolution3D, Dense, Dropout, Flatten, Concatenate, Merge, merge
 from keras.utils import np_utils # utilities for one-hot encoding of ground truth values
 from load_data import load
-from preprocess import smooth_data,normalize
+from preprocess import preprocess_data
 from metadata import prepare_metadata
 
 ###### get IDs and labels ######
 fn = '/home/yklocal/Downloads/OASIS3_noNA_ybin.csv'
 dirimg = '/home/yklocal/Downloads/Original_cropped_pkl2/'
-data = load(fn, dirimg)
+dirjac = '/home/yklocal/'
+data = load(fn, dirimg, dirjac)
 Y_train0 = data.y_train0
 Y_test0 = data.y_test0
 
@@ -24,17 +25,22 @@ Y_test0 = data.y_test0
 
 
 ###### smooth and normalize ######
-X_train0 = smooth_data(data.train_data_matrix, 1)
-X_train0 = normalize(X_train0)
-
+# X_train0 = smooth_data(data.train_data_matrix, 1)
+# X_train0 = normalize(X_train0)
+X_train0_pre = preprocess_data(data.train_data_matrix, 1)
+X_train0 =X_train0_pre.data
+X_test0_pre = preprocess_data(data.test_data_matrix, 1, mean=X_train0_pre.overallMean, sd=X_train0_pre.overallStd)
+X_test0 =X_test0_pre.data
 
 ###### set data #######
 X_train, Y_train = X_train0, Y_train0
-X_test, Y_test = data.test_data_matrix, Y_test0
+X_test, Y_test = X_test0, Y_test0
+X_train_jac = data.jac_train_data_matrix
+X_test_jac = data.jac_test_data_matrix
 
 ####Some Hyperparameters
 batch_size = 5
-num_epochs = 100
+num_epochs = 20
 num_classes = 2
 
 conv_depth_1 = 45
@@ -42,6 +48,7 @@ conv_depth_2 = 60
 conv_depth_3 = 84
 conv_depth_4 = 128
 conv_depth_5 = 256
+conv_depth_6 = 512
 drop_prob_1 = 0.5
 drop_prob_2 = 0.25
 hidden_size = 512
@@ -50,31 +57,33 @@ hidden_size = 512
 
 
 number_of_classes = 2
-Y_train = np_utils.to_categorical(Y_train, number_of_classes)
+Y_train = np_utils.to_categorical(Y_train[0:196], number_of_classes)
 Y_test = np_utils.to_categorical(Y_test, number_of_classes)
 
-# X_train = np.expand_dims(X_train0, axis=0)
-train_set = np.zeros((X_train0.shape[0], 1, X_train0.shape[1],X_train0.shape[2],X_train0.shape[3]))
-for h in xrange(X_train0.shape[0]):
-    train_set[h][0][:][:][:] = X_train0[h,:,:,:]
-
-test_set = np.zeros((X_test.shape[0], 1, X_test.shape[1],X_test.shape[2],X_test.shape[3]))
-for h in xrange(X_test.shape[0]):
-    test_set[h][0][:][:][:] = X_test[h,:,:,:]
+X_train = np.expand_dims(X_train0, axis=4)
+# train_set = np.zeros((X_train0.shape[0], 1, X_train0.shape[1],X_train0.shape[2],X_train0.shape[3]))
+# for h in xrange(X_train0.shape[0]):
+#     train_set[h][0][:][:][:] = X_train0[h,:,:,:]
+#
+# test_set = np.zeros((X_test.shape[0], 1, X_test.shape[1],X_test.shape[2],X_test.shape[3]))
+# for h in xrange(X_test.shape[0]):
+#     test_set[h][0][:][:][:] = X_test[h,:,:,:]
 
 # print(X_train.shape)
 
+train_set = X_train[0:196]
+test_set = np.expand_dims(X_test, axis=4)
 # inp = Input(shape=(X_train.shape[1], X_train.shape[2],X_train.shape[3],X_train.shape[4]))
-inp = Input(shape=(1, X_train0.shape[1],X_train0.shape[2],X_train0.shape[3]))
+inp = Input(shape=(X_train0.shape[1],X_train0.shape[2],X_train0.shape[3],1))
 
-conv_1_1 = Convolution3D(conv_depth_1, 7,7,7, padding='same',activation='relu',subsample=(2,2,2))(inp)
-conv_1_2 = Convolution3D(conv_depth_1, 6,6,6, padding='same',activation='relu',subsample=(2,2,2))(inp)
+# conv_1_1 = Convolution3D(conv_depth_1, 7,7,7, padding='same',activation='relu',subsample=(2,2,2))(inp)
+# conv_1_2 = Convolution3D(conv_depth_1, 6,6,6, padding='same',activation='relu',subsample=(2,2,2))(inp)
 conv_1_3 = Convolution3D(conv_depth_1, 5,5,5, padding='same',activation='relu',subsample=(2,2,2))(inp)
 
 # conv_1 = Concatenate([conv_1_1, conv_1_2, conv_1_3])
-conv_1 = merge([conv_1_1, conv_1_2, conv_1_3], mode='concat', concat_axis=1)
+# conv_1 = merge([conv_1_1, conv_1_2, conv_1_3], mode='concat', concat_axis=1)
 
-drop_1 = Dropout(drop_prob_1)(conv_1)
+drop_1 = Dropout(drop_prob_1)(conv_1_3)
 
 conv_2 = Convolution3D(conv_depth_2, 5,5,5, padding='same',activation='relu',subsample=(2,2,2))(drop_1)
 drop_2 = Dropout(drop_prob_1)(conv_2)
@@ -82,19 +91,31 @@ drop_2 = Dropout(drop_prob_1)(conv_2)
 conv_3 = Convolution3D(conv_depth_3, 5,5,5, padding='same',activation='relu',subsample=(2,2,2))(drop_2)
 drop_3 = Dropout(drop_prob_1)(conv_3)
 
-conv_4 = Convolution3D(conv_depth_4, 3,3,3, padding='same',activation='relu',subsample=(2,2,2))(drop_3)
-drop_4 = Dropout(drop_prob_1)(conv_4)
+# conv_4 = Convolution3D(conv_depth_4, 3,3,3, padding='same',activation='relu',subsample=(2,2,2))(drop_3)
+# drop_4 = Dropout(drop_prob_1)(conv_4)
+#
+# conv_5 = Convolution3D(conv_depth_5, 3,3,3, padding='same',activation='relu',subsample=(2,2,2))(drop_4)
+# drop_5 = Dropout(drop_prob_1)(conv_5)
+#
+# conv_6 = Convolution3D(conv_depth_5, 3,3,3, padding='same',activation='relu',subsample=(2,2,2))(drop_5)
+# drop_6 = Dropout(drop_prob_1)(conv_6)
 
-conv_5 = Convolution3D(conv_depth_5, 3,3,3, padding='same',activation='relu',subsample=(2,2,2))(drop_4)
-drop_5 = Dropout(drop_prob_1)(conv_5)
+flat = Flatten()(drop_3)
+# hidden = Dense(hidden_size, activation='relu')(flat)
+hidden = Dense(1028, activation='relu')(flat)
+drop_7 = Dropout(drop_prob_2)(hidden)
 
-flat = Flatten()(drop_5)
-hidden = Dense(hidden_size, activation='relu')(flat)
-drop_6 = Dropout(drop_prob_2)(hidden)
-
-out = Dense(num_classes,activation='softmax', name='feat')(drop_6)
+out = Dense(num_classes,activation='softmax', name='feat')(drop_7)
 
 model = Model(inputs=inp,outputs=out)
+
+print(inp._keras_shape)
+print(conv_1_3._keras_shape)
+print(conv_2._keras_shape)
+print(conv_3._keras_shape)
+# print(conv_4._keras_shape)
+# print(conv_5._keras_shape)
+# print(conv_6._keras_shape)
 
 intermediate_layer_model = Model(inputs=model.input,
                                  outputs=model.get_layer('feat').output) ### pull out features
@@ -104,22 +125,31 @@ intermediate_output_test = intermediate_layer_model.predict(test_set)
 
 
 
-model.compile(loss='categorical_crossentropy',
+model.compile(loss='binary_crossentropy',
              optimizer='adam',
-             metrics=['accuracy'])
+             metrics=['accuracy', 'mse'])
 
 # X_train_new, X_val_new, y_train_new,y_val_new = train_test_split(train_set, Y_train, test_size=0.2, random_state=4)
 # print(Y_train.shape)
-model.fit(train_set,Y_train,
+model.fit(train_set,Y_train, shuffle=True,
          batch_size=batch_size,epochs=num_epochs,
-         verbose=1, validation_data=(test_set, Y_test))
-model.evaluate(test_set, Y_test, verbose=1)
+         verbose=1, validation_split=0.1)
+print(model.evaluate(test_set, Y_test, verbose=1))
+# print(train_set)
+# validation_data=(test_set, Y_test)
+# print(Y_train)
+# print('\n now test')
+# print(Y_test)
+# print('\n now predict')
+# print(model.predict(test_set))
 
+modelname = '3epochs'
+model.save("oasis_{0}.h5".format(modelname))
 
 ############# prepare metadata #######
-demogfn = '/Users/yeunkim/Data/OASIS/csv/X_DataFrame2.csv'
-measures = '/Users/yeunkim/Data/OASIS/csv/Y_DataFrame.csv'
-
-metadata =  prepare_metadata(demogfn, measures,data)
-
-print(intermediate_output.shape)
+# demogfn = '/Users/yeunkim/Data/OASIS/csv/X_DataFrame2.csv'
+# measures = '/Users/yeunkim/Data/OASIS/csv/Y_DataFrame.csv'
+#
+# metadata =  prepare_metadata(demogfn, measures,data)
+#
+# print(intermediate_output.shape)

bin/preprocess.py
@@ -6,21 +6,48 @@ Performs Gaussian smoothing and normalization
 import numpy as np
 from scipy.ndimage import filters
 
+class preprocess_data(object):
 
-def smooth_data(data_matrix, smooth_sz):
-    dataSmooth = np.zeros((data_matrix.shape[0],data_matrix.shape[1],data_matrix.shape[2],data_matrix.shape[3]))
-    for i in range(data_matrix.shape[0]):
-      dataSmooth[i] = filters.gaussian_filter(data_matrix[i,:,:,:],smooth_sz)
-    return dataSmooth
+    def __init__(self, data_matrix, smooth_sz, **keyword_parameters):
+        self.overallMean = 0
+        self.overallStd = 0
+        self.data = []
+        self.dataSmooth = []
+
+        self.smooth_data(data_matrix, smooth_sz)
+        self.normalize(data_matrix, **keyword_parameters)
 
 
-def normalize(data_matrix):
-    data = np.zeros((data_matrix.shape[0], data_matrix.shape[1], data_matrix.shape[2], data_matrix.shape[3]))
-    overallMean = np.mean(data_matrix, axis=None)
-    overallStd = np.std(data_matrix, axis=None)
-    for i in range(data_matrix.shape[0]):
-        tmp = (data_matrix[i] - np.mean(data_matrix[i])) / np.std(data_matrix[i])
-        tmp_data = (tmp - overallMean) / overallStd
-        data[i] = tmp
-    return data
+    def smooth_data(self,data_matrix, smooth_sz):
+        dataSmooth = np.zeros((data_matrix.shape[0],data_matrix.shape[1],data_matrix.shape[2],data_matrix.shape[3]))
+        for i in range(data_matrix.shape[0]):
+          dataSmooth[i] = filters.gaussian_filter(data_matrix[i,:,:,:],smooth_sz)
+        self.dataSmooth = dataSmooth
 
+
+    def normalize(self, data_matrix, **keyword_parameters):
+        data = np.zeros((data_matrix.shape[0], data_matrix.shape[1], data_matrix.shape[2], data_matrix.shape[3]))
+
+        if "mean" in keyword_parameters:
+            for i in range(data_matrix.shape[0]):
+                tmp = (self.dataSmooth[i] - np.mean(self.dataSmooth[i])) / np.std(self.dataSmooth[i])
+                # tmp_data = (tmp - self.overallMean) / self.overallStd
+                data[i] = tmp
+            # self.overallMean = np.mean(data, axis=None)
+            # self.overallStd = np.std(data, axis=None)
+            data = (data - keyword_parameters['mean']) / keyword_parameters['sd']
+
+        else:
+
+            for i in range(data_matrix.shape[0]):
+                tmp = (self.dataSmooth[i] - np.mean(self.dataSmooth[i])) / np.std(self.dataSmooth[i])
+                # tmp_data = (tmp - self.overallMean) / self.overallStd
+                data[i] = tmp
+            self.overallMean = np.mean(data, axis=None)
+            self.overallStd = np.std(data, axis=None)
+
+            data = (data - self.overallMean) / self.overallStd
+            # print(data)
+        # return data
+        # print(data)
+        self.data = data